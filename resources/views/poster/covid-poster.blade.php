883d98129209ab4b392af3fe5026013b3b75cdd7
Amam Dewan <cybercrafttech@gmail.com>
2020-04-10 21:58:27 +0600

    Mobile view Update

--- resources/views/poster/covid-poster.blade.php
+++ resources/views/poster/covid-poster.blade.php
@@ -267,7 +267,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Covid poster</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.2.0/tailwind.min.css">
     <style>
